be69d98c924bf754cb1d7c9d055fee040bcf6924
Nursetiana Situmorang <45727455+nursetiana09@users.noreply.github.com>
2018-12-30 10:39:25 +0700

    Add files via upload

---
+++ mengubah fungsi sebanyak n kali.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>>
+using namespace std;
+
+int main ()
+{
+    int i,n;
+    cout<<"Menampilkan Kata Algoritma dan Pemrograman sebanyak n kali"<<endl;
+
+    cout<<"Masukan Jumlah yang diinginkan : ";
+    cin>>n;
+
+    for(i=1; i<=n; ++i)
+    {
+        cout<<"Algoritma dan Pemrograman"<<endl;
+    }
+    getch ();
+}
+
